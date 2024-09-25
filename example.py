e9300ffe10075f38d091ef3eeaf344915843e261
Yibo Wei <david_wyb2001@outlook.com>
2024-09-25 16:47:22 -0700

    Fix output comment

--- example.py
+++ example.py
@@ -16,4 +16,4 @@ print(a, b)  # 1 2
 fmt = U8() | (lambda length: U8() + Bytes(length - 1))
 data = b"\x03\x01\x02\x03" + b"\x05\xff\x01\x02\x03\x04"
 length, dtype, payload, _ = fmt.unpack(data).expand()
-print(length, dtype, payload)  # 3 1 b'\x02\x03' b'\xff\x01\x02\x03\x04'
+print(length, dtype, payload)  # 3 1 b'\x02\x03'
